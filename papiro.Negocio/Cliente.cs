5dc4411b47446bf4fe82857dbd278406be8399e0
Ricardo <ricardomarinpalacios@gmail.com>
2018-09-07 16:04:43 -0300

    PapiroWeb 1.0.0

---
+++ papiro.Negocio/Cliente.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace papiro.Negocio
+{
+    public class Cliente
+    {
+        private int id_cliente;
+
+        public int IdCliente
+        {
+            get { return id_cliente; }
+            set { id_cliente = value; }
+        }
+        private string _rut;
+
+        public string Rut
+        {
+            get { return _rut; }
+            set { _rut = value; }
+        }
+        private string _nombreCliente;
+
+        public string NombreCliente
+        {
+            get { return _nombreCliente; }
+            set { _nombreCliente = value; }
+        }
+        private string _direccion;
+
+        public string Direccion
+        {
+            get { return _direccion; }
+            set { _direccion = value; }
+        }
+
+        //Agregar comuna como tabla aparte
+        private string _telefono;
+
+        public string Telefono
+        {
+            get { return _telefono; }
+            set { _telefono = value; }
+        }
+
+
+        private int _idComuna;
+
+        public int IdComuna
+        {
+            get { return _idComuna; }
+            set { _idComuna = value; }
+        }
+
+    }
+}
---
+++ papiro.Negocio/Usuario.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace papiro.Negocio
+{
+    public class Usuario
+    {
+        private string _nombreUsuario;
+
+        public string NombreUsuario
+        {
+            get { return _nombreUsuario; }
+            set { _nombreUsuario = value; }
+        }
+        private string _password;
+
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+        private string _nombres;
+
+        public string Nombres
+        {
+            get { return _nombres; }
+            set { _nombres = value; }
+        }
+        private string _apellidos;
+
+        public string Apellidos
+        {
+            get { return _apellidos; }
+            set { _apellidos = value; }
+        }
+        private int _idDepartamento;
+
+        public int IdDepartamento
+        {
+            get { return _idDepartamento; }
+            set { _idDepartamento = value; }
+        }
+
+    }
+}
